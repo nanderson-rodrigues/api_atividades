5c5613dc666ee14fdb205693572b45ffad9784ba
Nanderson Rodrigues <nandersondsr@gmail.com>
2020-10-04 12:27:14 -0300

    adicionar models de pessoa e atividades e arquivo utils para manipular banco de dados

---
+++ utils.py
@@ -0,0 +1,26 @@
+from models import Pessoas
+
+def insere_pessoas():
+    pessoa = Pessoas(nome="Nanderson", idade=23)
+    print(pessoa)
+    pessoa.save()
+
+def consulta_pessoas():
+    pessoas = Pessoas.query.all()
+    print(pessoas)
+    pessoa = Pessoas.query.filter_by(nome='Rafael').first()
+    print(pessoa.idade)
+
+def altera_pessoa():
+    pessoa = Pessoas.query.filter_by(nome="Rafael").first()
+    pessoa.idade = 21
+    pessoa.save()
+
+def exclui_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Nanderson').first()
+    pessoa.delete()
+
+if __name__ == '__main__':
+    #insere_pessoas()
+    altera_pessoa()
+    consulta_pessoas()
