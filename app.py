dad91c556ea2e3135d9abae166c84ad8b0cfe153
Nanderson Rodrigues <nandersondsr@gmail.com>
2020-10-08 11:41:04 -0300

    inserir arquivo de requirements e mais metodos essenciais de api

--- app.py
+++ app.py
@@ -1,9 +1,85 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+from flask_restful import Resource, Api
+from models import Pessoas, Atividades
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app = Flask(__name__)
+api = Api(app)
+
+class Pessoa(Resource):
+
+    def get(self, nome):
+        pessoa = Pessoas.query.filter_by(nome=nome).first()
+        try:
+            response = {
+                'nome': pessoa.nome,
+                'idade': pessoa.idade,
+                'id': pessoa.id
+            }
+        except AttributeError:
+            response = {
+                'status': 'erro',
+                'mesagem': 'Pessoa nao encontrada'
+            }
+        return response
+
+    def put(self, nome):
+        pessoa = Pessoas.query.filter_by(nome=nome).first()
+        dados = request.json
+        if 'nome' in dados:
+            pessoa.nome = dados['nome']
+        if 'idade' in dados:
+            pessoa.idade = dados['idade']
+        pessoa.save()
+        response = {
+            'id': pessoa.id,
+            'nome': pessoa.nome,
+            'idade': pessoa.idade
+        }
+        return response
+
+    def delete(self, id):
+        pessoa = Pessoas.query.filter_by(id=id).first()
+        mensagem = "Pessoa {} excluida com sucesso".format(pessoa.nome)
+        pessoa.delete()
+        return {'status': 'Sucesso', 'mensagem': mensagem}
+
+class ListaPessoas(Resource):
+    def get(self):
+        pessoas = Pessoas.query.all()
+        response = [ {'id':i.id, 'nome': i.nome, 'idade': i.idade}  for i in pessoas]
+        return response
+    def post(self):
+        dados = request.json
+        pessoa = Pessoas(nome=dados['nome'], idade=dados['idade'])
+        pessoa.save()
+        response = {
+            'id': pessoa.id,
+            'nome': pessoa.nome,
+            'idade': pessoa.idade
+        }
+        return response
+
+class ListaAtividades(Resource):
+    def get(self):
+        atividades = Atividades.query.all()
+        response = [{'id' : i.id, 'nome': i.nome, 'pessoa': i.pessoa.nome} for i in atividades]
+        return response
+
+    def post(self):
+        dados = request.json
+        pessoa = Pessoas.query.filter_by(nome=dados['pessoa']['nome']).first()
+        atividade = Atividades(nome=dados["nome"], pessoa=pessoa)
+        atividade.save()
+        response = {
+            'pessoa': atividade.pessoa.nome,
+            'nome': atividade.nome,
+            'id': atividade.id
+        }
+        return response
+
+api.add_resource(Pessoa, '/pessoa/<int:id>/')
+api.add_resource(ListaPessoas, '/pessoa/')
+api.add_resource(ListaAtividades, '/atividades/')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
